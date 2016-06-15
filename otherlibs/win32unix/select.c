535b3a623d92c69a8ef7224d3bb5184f800f6f56
David Scott <dave@recoil.org>
2016-06-15 11:50:18 +0100

    Windows: avoid calling select() with more than FD_SETSIZE fds
    This is similar to "PR#5563: harden Unix.select against file descriptors above FD_SETSIZE"
    except that we can still handle these larger lists using the more generic
    (but more complicated) codepath.
    
    The MSDN documentation for select states that "The variable FD_SETSIZE
    determines the maximum number of descriptors in a set.":
    
      https://msdn.microsoft.com/en-gb/library/windows/desktop/ms740141(v=vs.85).aspx
    
    If we have a list of fds which is too large to be used in select(),
    fall back to the generic codepath which is used for select()ing on multiple
    types of handles at once.
    
    Note the default FD_SETSIZE is 64, so it's fairly easy to hit this limit.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- otherlibs/win32unix/select.c
+++ otherlibs/win32unix/select.c
@@ -909,9 +909,12 @@ static int fdlist_to_fdset(value fdlist, fd_set *fdset)
 {
   value l, c;
   FD_ZERO(fdset);
+  int used = 0;
   for (l = fdlist; l != Val_int(0); l = Field(l, 1)) {
     c = Field(l, 0);
     if (Descr_kind_val(c) == KIND_SOCKET) {
+      used++;
+      if (used > FD_SETSIZE) return 0;
       FD_SET(Socket_val(c), fdset);
     } else {
       DEBUG_PRINT("Non socket value encountered");
